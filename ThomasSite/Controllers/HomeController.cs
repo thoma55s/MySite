0f1348041edea0f582b509356cf4f9aff4bea0e4
Thomas Sager <thomas.sager@waystar.com>
2019-01-25 13:15:00 -0500

    Routing

--- ThomasSite/Controllers/HomeController.cs
+++ ThomasSite/Controllers/HomeController.cs
@@ -4,16 +4,17 @@ namespace ThomasSite.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
-
+        [Route("Resume")]
         public ViewResult Resume()
         {
             return View();
         }
-
+        [Route("ACM")]
         public ViewResult Acm()
         {
             return View();
