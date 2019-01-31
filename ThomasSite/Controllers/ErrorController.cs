4e95eaa1b80fd07164038d4aec938862e4d58c34
Thomas Sager <thomas.sager@waystar.com>
2019-01-31 09:19:53 -0500

    Error Model

--- ThomasSite/Controllers/ErrorController.cs
+++ ThomasSite/Controllers/ErrorController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Linq;
+using ThomasSite.Models;
 
 namespace ThomasSite.Controllers
 {
     public class ErrorController : Controller
     {
+        Error[] errorArray = 
+        {
+            new Error {ID = 404, Message = "Resource not found." },
+            new Error {ID = 500, Message = "Internal server error." }
+        };
         public ActionResult Error(int statusCode)
         {
             Response.StatusCode = statusCode;
-            ViewBag.StatusCode = statusCode + "";
-            return View();
+            ViewBag.StatusCode = statusCode;
+            
+            return View(errorArray.SingleOrDefault(item => item.ID == statusCode));
         }
     }
 }
